e690f2f36f1684a6c5b7bdc9a21209921733068c
Matt Robinson <matt@tui.co.uk>
2011-05-05 12:00:41 +0100

    Directors bundle

---
+++ src/Tui/DirectorsBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tui\DirectorsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+  
+    /**
+     * @extra:Route('/', name="home")
+     * @extra:Template()
+     */
+  
+    public function indexAction()
+    {
+
+    }
+}
---
+++ src/Tui/DirectorsBundle/TuiDirectorsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tui\DirectorsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TuiDirectorsBundle extends Bundle
+{
+}
