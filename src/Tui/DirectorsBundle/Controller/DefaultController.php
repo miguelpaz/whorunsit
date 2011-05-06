ab8296d7f365232900ede3c266140b870dbcede0
Matt Robinson <matt@tui.co.uk>
2011-05-06 14:25:03 +0100

    Make search work
    *  Twig text extension enabled
    * Set up _formats in the search_all route (default html)
    * Connected the search form in the base template to the search action
    * Search template displays results
    * Search action (needs refactoring, but works)

--- src/Tui/DirectorsBundle/Controller/DefaultController.php
+++ src/Tui/DirectorsBundle/Controller/DefaultController.php
@@ -30,21 +30,65 @@ class DefaultController extends Controller
 
     
     /**
-     * @extra:Route("/search", name="search")
+     * @extra:Route("/search/all.{_format}", name="search_all", defaults={"_format" = "html"})
      * @extra:Template()
      */
   
     public function searchAction()
     {
+        $dbh   = $this->get('database_connection');
+        $em    = $this->get('doctrine.orm.entity_manager');
         $query = $this->get('request')->get('q');
-      
-        $sphinx = $this->get('search');
+        
+        if (!trim($query))
+        {
+            return $this->redirect($this->generateUrl('home'));
+        }
+        
+        // Get the search service and execute the query
+        $sphinx     = $this->get('search');
         $appointees = $sphinx->Query($query, 'appointees');
-        $companies = $sphinx->Query($query, 'companies');
-        $results = array('appointees' => $appointees, 'companies' => $companies);
-        die(var_dump($results));
+        $companies  = $sphinx->Query($query, 'companies');
+
+
+        // Gather appointee document ids, turn them into appointee ids
+        $appointee_ids = array();
+        if ($appointees['total_found'])
+        {
+          $appointee_ids = array_map(function($v){return $v['id'];}, $appointees['matches']);
+
+          $ids = $dbh->fetchAll('SELECT ch_number FROM sphinx_appointee_index WHERE document_id IN ('.join($appointee_ids,',').')');
+
+          $ids = array_map(function($v){return $v['ch_number'];}, $ids);
+
+          $ex = $em->getExpressionBuilder();
+          $q = $em->createQuery('SELECT a FROM TuiDirectorsBundle:Appointee a WHERE '.$ex->in('a.id', $ids));
+          $appointees = $q->getArrayResult();
+          
+          $appointees = array_map(function($v){ if ($v['dateOfBirth']) $v['dateOfBirth'] = $v['dateOfBirth']->format('Y'); return $v; }, $appointees);
+        }
+
+
+        // Gather company document ids, turn them into company ids
+        $company_ids = array();
+        if ($companies['total_found'])
+        {
+          $company_ids = array_map(function($v){return $v['id'];}, $companies['matches']);
+
+          $ids = $dbh->fetchAll('SELECT ch_number FROM sphinx_company_index WHERE document_id IN ('.join($company_ids,',').')');
+
+          $ids = array_map(function($v){return $v['ch_number'];}, $ids);
+
+          $ex = $em->getExpressionBuilder();
+          $q = $em->createQuery('SELECT c FROM TuiDirectorsBundle:Company c WHERE '.$ex->in('c.id', $ids));
+          $companies = $q->getArrayResult();
+        }
+
+
+
+
       
-        return array();
+        return array('appointees' => $appointees, 'companies' => $companies, 'query' => $query);
     }
 
 
