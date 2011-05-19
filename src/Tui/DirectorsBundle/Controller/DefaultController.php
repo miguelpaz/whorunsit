5e07a43c193ecb597abf40608b72f71b2533f0ec
Matt Robinson <matt@tui.co.uk>
2011-05-19 13:11:46 +0100

    Paginate search results, show more (and better) metadata

--- src/Tui/DirectorsBundle/Controller/DefaultController.php
+++ src/Tui/DirectorsBundle/Controller/DefaultController.php
@@ -49,13 +49,38 @@ class DefaultController extends Controller
         
         // Get the search service and execute the query
         $sphinx             = $this->get('search');
+
+        // Configure pagination
+        $page = (int) $this->get('request')->get('page');
+        $page = $page ?: 1;
+        $page_info = array(
+            'query'       => $query,
+            'page_length' => $this->container->getParameter('search_page_length'),
+            'page'        => $page,
+            'offset'      => ($page - 1) * $this->container->getParameter('search_page_length'),
+        );
+
+        // Do the search
+        $sphinx->setLimits($page_info['offset'], $page_info['page_length']);
         $appointees_results = $sphinx->Query($query, 'appointees');
         $companies_results  = $sphinx->Query($query, 'companies');
 
+        // Fill out remainining pagination info
+        $page_info['appointees_found'] = $appointees_results['total'];
+        $page_info['companies_found']  = $companies_results['total'];
+        $page_info['appointees_end']   = (($page_info['offset'] + $page_info['page_length']) > $appointees_results['total']) ? $appointees_results['total'] : $page_info['offset'] + $page_info['page_length'];
+        $page_info['companies_end']    = (($page_info['offset'] + $page_info['page_length']) > $companies_results['total']) ? $companies_results['total'] : $page_info['offset'] + $page_info['page_length'];
+        if ($appointees_results['total'] > $companies_results['total'])
+        {
+            $page_info['has_more_pages'] = ($appointees_results['total'] > $page_info['appointees_end']);
+        } else {
+            $page_info['has_more_pages'] = ($companies_results['total'] > $page_info['companies_end']);
+        }
+        
 
         // Gather appointee document ids, turn them into appointee ids
         $appointee_ids = array();
-        if ($appointees_results['total_found'])
+        if (isset($appointees_results['matches']))
         {
           $appointee_ids = array_map(function($v){return $v['id'];}, $appointees_results['matches']);
 
@@ -64,14 +89,31 @@ class DefaultController extends Controller
           $ids = array_map(function($v){return $v['ch_number'];}, $ids);
 
           $ex = $em->getExpressionBuilder();
-          $q = $em->createQuery('SELECT a FROM TuiDirectorsBundle:Appointee a WHERE '.$ex->in('a.id', $ids));
+          $q = $em->createQuery('SELECT a
+              FROM TuiDirectorsBundle:Appointee a
+              WHERE '.$ex->in('a.id', $ids).'
+              ORDER BY a.surname, a.forenames
+              ');
           $appointees = $q->getResult();
         }
 
 
+        // Get truncated companies for appointees
+        $r = $em->getRepository('TuiDirectorsBundle:Appointee');
+        
+        $appointee_companies = array();
+        foreach($appointees as $a)
+        {
+            $appointee_companies[ $a->getId() ] = $r->getAbbreviatedCompanies($a, 5);
+        }
+        
+
+
+
+
         // Gather company document ids, turn them into company ids
         $company_ids = array();
-        if ($companies_results['total_found'])
+        if (isset($companies_results['matches']))
         {
           $company_ids = array_map(function($v){return $v['id'];}, $companies_results['matches']);
 
@@ -83,6 +125,17 @@ class DefaultController extends Controller
           $q = $em->createQuery('SELECT c FROM TuiDirectorsBundle:Company c WHERE '.$ex->in('c.id', $ids));
           $companies = $q->getResult();
         } 
+
+
+        // Get truncated appointees for companies
+        $r = $em->getRepository('TuiDirectorsBundle:Company');
+        
+        $company_appointees = array();
+        foreach($companies as $c)
+        {
+            $company_appointees[ $c->getId() ] = $r->getAbbreviatedAppointees($c, 5);
+        }
+
 
 
         if ($this->get('request')->getRequestFormat() == 'json')
@@ -138,7 +191,7 @@ class DefaultController extends Controller
 
 
       
-        return array('appointees' => $appointees, 'companies' => $companies, 'query' => $query);
+        return array('appointees' => $appointees, 'companies' => $companies, 'query' => $query, 'appointee_companies' => $appointee_companies, 'company_appointees' => $company_appointees, 'page_info' => $page_info);
     }
 
 
