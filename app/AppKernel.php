25b32c6549aecd3f48d583831c1d642ef7c64a09
Matt Robinson <matt@tui.co.uk>
2011-05-25 10:54:29 +0100

    Re-enable JMSSecurityExtraBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            // new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
 
             new Tui\DirectorsBundle\TuiDirectorsBundle(),
