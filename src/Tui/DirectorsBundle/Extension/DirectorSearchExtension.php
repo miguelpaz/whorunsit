3aff2ff62fed6c7d19c714562c0bf9d5e45bb8ab
Matt Robinson <matt@tui.co.uk>
2011-06-13 14:20:09 +0100

    Properly inject dependencies into extension

--- src/Tui/DirectorsBundle/Extension/DirectorSearchExtension.php
+++ src/Tui/DirectorsBundle/Extension/DirectorSearchExtension.php
@@ -2,29 +2,33 @@
 
 namespace Tui\DirectorsBundle\Extension;
 
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\Routing\Router;
+use Symfony\Bundle\DoctrineBundle\Registry;
 
-
-class DirectorSearchExtension extends ContainerAware
+class DirectorSearchExtension
 {
     protected $search = null;
+    protected $dbh    = null;
+    protected $router = null;
+    protected $em     = null;
+    
     
     protected $page_length = 100;
     
     
     
-    public function __construct(\SphinxClient $search)
+    public function __construct(\SphinxClient $search, Connection $connection, Router $router, Registry $doctrine)
     {
         $this->search = $search;
+        $this->dbh    = $connection;
+        $this->router = $router;
+        $this->em     = $doctrine->getEntityManager();
     }
     
     
     public function searchAll($query, $page = 1)
     {
-        $dbh   = $this->container->get('database_connection');
-        $em    = $this->container->get('doctrine')->getEntityManager();
-        
-        
         
         // Configure pagination
         $page_info = array(
@@ -58,18 +62,18 @@ class DirectorSearchExtension extends ContainerAware
         {
           $appointee_ids = array_map(function($v){return $v['id'];}, $appointees_results['matches']);
 
-          $ids = $dbh->fetchAll('SELECT ch_number FROM sphinx_appointee_index WHERE document_id IN ('.join($appointee_ids,',').')');
+          $ids = $this->dbh->fetchAll('SELECT ch_number FROM sphinx_appointee_index WHERE document_id IN ('.join($appointee_ids,',').')');
 
           $ids = array_map(function($v){return $v['ch_number'];}, $ids);
 
-          $ex = $em->getExpressionBuilder();
-          $q = $em->createQuery('SELECT a
+          $ex = $this->em->getExpressionBuilder();
+          $q = $this->em->createQuery('SELECT a
               FROM TuiDirectorsBundle:Appointee a
               WHERE '.$ex->in('a.id', $ids));
           $appointees = $q->getResult();
           
           // Get truncated companies for appointees
-          $r = $em->getRepository('TuiDirectorsBundle:Appointee');
+          $r = $this->em->getRepository('TuiDirectorsBundle:Appointee');
 
           
           foreach($appointees as $a)
@@ -87,17 +91,17 @@ class DirectorSearchExtension extends ContainerAware
         {
           $company_ids = array_map(function($v){return $v['id'];}, $companies_results['matches']);
 
-          $ids = $dbh->fetchAll('SELECT ch_number FROM sphinx_company_index WHERE document_id IN ('.join($company_ids,',').')');
+          $ids = $this->dbh->fetchAll('SELECT ch_number FROM sphinx_company_index WHERE document_id IN ('.join($company_ids,',').')');
 
           $ids = array_map(function($v){return $v['ch_number'];}, $ids);
 
-          $ex = $em->getExpressionBuilder();
-          $q = $em->createQuery('SELECT c FROM TuiDirectorsBundle:Company c WHERE '.$ex->in('c.id', $ids));
+          $ex = $this->em->getExpressionBuilder();
+          $q = $this->em->createQuery('SELECT c FROM TuiDirectorsBundle:Company c WHERE '.$ex->in('c.id', $ids));
           $companies = $q->getResult();
 
 
           // Get truncated appointees for companies
-          $r = $em->getRepository('TuiDirectorsBundle:Company');
+          $r = $this->em->getRepository('TuiDirectorsBundle:Company');
 
           
           foreach($companies as $c)
@@ -134,7 +138,7 @@ class DirectorSearchExtension extends ContainerAware
           foreach($results['appointees'] as $a)
           {
             $out['appointees'][] = array(
-              'url'           => $this->container->get('router')->generate('appointee_show', array('_format' => 'json', 'id' => $a->getId()), true),
+              'url'           => $this->router->generate('appointee_show', array('_format' => 'json', 'id' => $a->getId()), true),
               'id'            => $a->getId(),
               'title'         => $a->getTitle(),
               'forenames'     => $a->getForenames(),
@@ -155,7 +159,7 @@ class DirectorSearchExtension extends ContainerAware
           {
             $out['companies'][] = array(
               'id'   => $c->getId(),
-              'url'  => $this->container->get('router')->generate('company_show', array('_format' => 'json', 'id' => $c->getId()), true),
+              'url'  => $this->router->generate('company_show', array('_format' => 'json', 'id' => $c->getId()), true),
               'name' => $c->getName(),
             );
           }
