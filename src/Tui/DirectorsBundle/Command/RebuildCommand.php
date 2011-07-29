6d0d2ef7d4c9dda9b983e79c8aa15103fe698c29
Matt Robinson <matt@tui.co.uk>
2011-07-29 14:12:33 +0100

    Initial work on porting the "rebuild" task from the original symfony 1 version

---
+++ src/Tui/DirectorsBundle/Command/RebuildCommand.php
@@ -0,0 +1,52 @@
+<?php
+namespace Tui\DirectorsBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+
+
+class RebuildCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('whorunsit:rebuild')
+            ->setDescription('Rebuild company, appointee, and appointment tables from data')
+            ->addArgument('dir', InputArgument::REQUIRED, 'Path to data files')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dir = $input->getArgument('dir');
+        
+        if (!is_dir($dir))
+        {
+            throw new \Exception('Invalid directory specified');
+        }
+
+        $finder = new Finder();
+        $finder->files()->in($dir)->name('*.txt');
+
+        foreach ($finder as $file) {
+            $this->parseFile($file, $output);
+        }
+
+    }
+    
+    
+    
+    protected function parseFile($file, $output)
+    {
+        $output->writeln(sprintf('<info>Parsing %s</info>', $file->getFilename()));
+
+        $parser = new chDirectorsParser($file->getRealpath());
+        $output->writeln((string)$parser.PHP_EOL);
+        
+      
+    }
+}
